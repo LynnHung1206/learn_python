c8f3b67b00ea6f0c2fce04032a30b5a33402a736
Lynn <lynn990908@gmail.com>
2025-02-16 18:47:54 +0800

    module

---
+++ basic/8_module.py
@@ -0,0 +1,11 @@
+import sys
+import module.geometry as geom
+
+print(sys.path)  # s.path 印出模組的搜尋路徑 載入模組時按照順序查找
+# sys.path.append("module") # 增加模組搜尋路徑
+# print(sys.path)
+# print(sys.platform)  # s.platform
+# print(sys.maxsize)  # s.maxsize
+
+print(geom.distance(1, 1, 5, 5))
+print(geom.slope(1, 2, 5, 6))
